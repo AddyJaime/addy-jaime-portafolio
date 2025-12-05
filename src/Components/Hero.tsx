b1e8fd71cb43eb25fe564d335e401bbf2c651274
addy <addyjaime@gmail.com>
2025-12-04 19:15:17 -0500

    .

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,23 +2,26 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen scroll-mt-24 flex items-center ">
-      <div className="w-full max-w-3xl mb-[120px]  md:px-10 lg:pl-24 xl:pl-32 text-left">
+     <section className="h-[85vh] scroll-mt-24 flex items-center">
+      <div className="w-full max-w-4xl mt-[-40px] md:px-10 lg:pl-24 xl:pl-32 text-left">
+        
         {/* Line 1: Presentation */}
-        <p className="text-base md:text-lg text-light tracking-[0.32em] uppercase mb-4">
+        <p className="text-base md:text-lg text-light tracking-[0.32em] uppercase mb-3">
           Hello, I&apos;m
         </p>
 
         {/* Line 2: Name */}
-        <h1 className="text-6xl md:text-8xl font-extrabold tracking-tight text-light">
+        <h1 className="text-7xl md:text-9xl font-extrabold tracking-tight text-light leading-[1.1]">
           Addy<span className="text-accent px-3">Jaime</span>
         </h1>
+
         {/* Line 3: Profession */}
         <p className="mt-6 text-xl md:text-2xl text-light max-w-3xl leading-relaxed">
           a software engineer with 3+ years of experience. I’m passionate about
           technology and everything we can create with it — always chasing
           innovation like it’s a never-ending game.
         </p>
+
         {/* Line 4: Buttons */}
         <div className="mt-12 flex flex-wrap gap-5 justify-start">
           <Link
